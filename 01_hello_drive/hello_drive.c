6bce33afbb6205972b8317c3095ca3cbc8801664
OliverCaoTora <doracaowei0511@gmail.com>
2026-06-29 18:01:01 -0400

    workspace

---
+++ 01_hello_drive/hello_drive.c
@@ -0,0 +1,29 @@
+#include <linux/mm.h>
+#include <linux/miscdevice.h>
+#include <linux/slab.h>
+#include <linux/vmalloc.h>
+#include <linux/mman.h>
+#include <linux/random.h>
+#include <linux/init.h>
+#include <linux/raw.h>
+#include <linux/tty.h>
+#include <linux/capability.h>
+#include <linux/ptrace.h>
+#include <linux/device.h>
+#include <linux/highmem.h>
+#include <linux/backing-dev.h>
+#include <linux/shmem_fs.h>
+#include <linux/splice.h>
+#include <linux/pfn.h>
+#include <linux/export.h>
+#include <linux/io.h>
+#include <linux/uio.h>
+
+#include <linux/uaccess.h>
+
+static const struct file_operations hello_drv = {
+    .owner = THIS_MODULE,
+    .read = hello_read,
+    .open = hello_open,
+    .write = hello_write,
+};
